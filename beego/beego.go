d24f2194e1fbc6c6da3433daa25a98774b88942f
Berny Linville <bernylinville@devopsthink.org>
2021-12-21 14:22:45 +0800

    update beego orm

--- beego/beego.go
+++ beego/beego.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"github.com/beego/beego/orm"
+	"fmt"
+
+	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql" // 导入数据库驱动
 )
 
@@ -14,4 +16,35 @@ type User struct {
 func init() {
 	// set default database
 	orm.RegisterDataBase("default", "mysql", "root:root@/my_db?charset=utf8", 30)
+
+	// register model
+	orm.RegisterModel(new(User))
+	//RegisterModel 也可以同时注册多个 model
+	//orm.RegisterModel(new(User), new(Profile), new(Post))
+
+	// create table
+	orm.RunSyncdb("default", false, true)
+}
+
+func main() {
+	o := orm.NewOrm()
+	user := User{Name: "test"}
+
+	// insert
+	id, err := o.Insert(&user)
+	fmt.Printf("ID: %d, ERR: %v\n", id, err)
+
+	// update
+	user.Name = "berny"
+	num, err := o.Update(&user)
+	fmt.Printf("NUM: %d, ERR: %v\n", num, err)
+
+	// read one
+	u := User{Id: user.Id}
+	err = o.Read(&u)
+	fmt.Printf("ERR: %v\n", err)
+
+	// delete
+	num, err = o.Delete(&u)
+	fmt.Printf("NUM: %d, ERR: %v\n", num, err)
 }
